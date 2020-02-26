cb059119be4df6e603ba1bad034fd3f1302094f1
Siyu Liu <siyuliu@dhcp-hol-4214.eduroam.cornell.edu>
2020-02-26 09:55:49 -0500

    initial commi

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express"); // common js module 
+// to import express by using require keyword
+// import express from 'express'; 
+// yes 2015 module and node won't support easily
+const app = express(); // a new app
+
+app.get("/", (req, res) => {
+	res.send({ hi: "there" });
+});
+
+// environment variable
+// figure out what Heroku provides
+// or when running on our machine, listen to 5000
+const PORT = porcess.env.PORT || 5000; 
+app.listen(PORT);
